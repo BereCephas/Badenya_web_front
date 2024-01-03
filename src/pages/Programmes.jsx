94a050fa8d4e970a3d184b02a45ac90fa6f04ae5
BereCephas <cephasbere0@gmail.com>
2024-01-03 13:07:18 +0000

    POC complet

--- src/pages/Programmes.jsx
+++ src/pages/Programmes.jsx
@@ -27,41 +27,61 @@ export default function Programme() {
           </AccordionHeader>
           <AccordionBody>
             <div className="h-full flex justify-center items-center flex-col  w-full">
-              {/* <div className="grid grid-cols-5 grid-rows-8 gap-4 h-full w-full bg-beige place-items-center">
-                <div>1</div> <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>9</div>
-                <div>10</div>
-                <div>11</div>
-                <div>12</div>
-                <div>13</div>
-                <div>14</div> <div>15</div>
-                <div>16</div> <div>17</div> <div>18</div>
-                <div>19</div> <div>20</div> <div>21</div> <div>22</div>{" "}
-                <div>23</div> <div>24</div>
-                <div>25</div>
-                <div>26</div>
-                <div>27</div>
-                <div>28</div>
-                <div>29</div>
-                <div>30</div>
-                <div>31</div>
-                <div>32</div>
-                <div>33</div>
-                <div>34</div>
-                <div>35</div>
-                <div>36</div>
-                <div>37</div>
-                <div>38</div>
-                <div>39</div>
-                <div>40</div>
-              </div> */}
-              Pas de devoir
+              <table class="table-auto min-w-full bg-white border border-gray-300">
+                <thead>
+                  <tr>
+                    <th class="py-2">#</th>
+                    <th class="py-2">Classes</th>
+                    <th class="py-2">Mercredi 12</th>
+                    <th class="py-2">vendredi 14</th>
+                    <th class="py-2">mercredi 19</th>
+                    <th class="py-2">vendredi 21</th>
+                    <th class="py-2">Mercredi 26</th>
+                    <th class="py-2">vendredi 28</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="border px-4 py-2">1</td>
+                    <td class="border px-4 py-2">6e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">2</td>
+                    <td class="border px-4 py-2">5e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">3</td>
+                    <td class="border px-4 py-2">4e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">4</td>
+                    <td class="border px-4 py-2">3e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </AccordionBody>
         </Accordion>
@@ -71,41 +91,64 @@ export default function Programme() {
           </AccordionHeader>
           <AccordionBody>
             <div className="h-full flex justify-center items-center flex-col  w-full">
-              {/* <div className="grid grid-cols-5 grid-rows-8 gap-4 h-full w-full bg-beige place-items-center">
-                <div>1</div> <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>9</div>
-                <div>10</div>
-                <div>11</div>
-                <div>12</div>
-                <div>13</div>
-                <div>14</div> <div>15</div>
-                <div>16</div> <div>17</div> <div>18</div>
-                <div>19</div> <div>20</div> <div>21</div> <div>22</div>{" "}
-                <div>23</div> <div>24</div>
-                <div>25</div>
-                <div>26</div>
-                <div>27</div>
-                <div>28</div>
-                <div>29</div>
-                <div>30</div>
-                <div>31</div>
-                <div>32</div>
-                <div>33</div>
-                <div>34</div>
-                <div>35</div>
-                <div>36</div>
-                <div>37</div>
-                <div>38</div>
-                <div>39</div>
-                <div>40</div>
-              </div> */}
-              Pas de devoir
+              <table class="table-auto min-w-full bg-white border border-gray-300">
+                <thead>
+                  <tr>
+                    <th class="py-2">#</th>
+                    <th class="py-2">Classes</th>
+                    <th class="py-2">Mercredi 12</th>
+                    <th class="py-2">vendredi 14</th>
+                    <th class="py-2">mercredi 19</th>
+                    <th class="py-2">vendredi 21</th>
+                    <th class="py-2">Mercredi 26</th>
+                    <th class="py-2">vendredi 28</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="border px-4 py-2">1</td>
+                    <td class="border px-4 py-2">2nde</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">2</td>
+                    <td class="border px-4 py-2">5e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                    <td class="border px-4 py-2">Philo</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">3</td>
+                    <td class="border px-4 py-2">4e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                    <td class="border px-4 py-2">Philo</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">4</td>
+                    <td class="border px-4 py-2">3e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                    <td class="border px-4 py-2">Philo</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </AccordionBody>
         </Accordion>
@@ -115,41 +158,64 @@ export default function Programme() {
           </AccordionHeader>
           <AccordionBody>
             <div className="h-full flex justify-center items-center flex-col  w-full">
-              {/* <div className="grid grid-cols-5 grid-rows-8 gap-4 h-full w-full bg-beige place-items-center">
-                <div>1</div> <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-                <div>9</div>
-                <div>10</div>
-                <div>11</div>
-                <div>12</div>
-                <div>13</div>
-                <div>14</div> <div>15</div>
-                <div>16</div> <div>17</div> <div>18</div>
-                <div>19</div> <div>20</div> <div>21</div> <div>22</div>{" "}
-                <div>23</div> <div>24</div>
-                <div>25</div>
-                <div>26</div>
-                <div>27</div>
-                <div>28</div>
-                <div>29</div>
-                <div>30</div>
-                <div>31</div>
-                <div>32</div>
-                <div>33</div>
-                <div>34</div>
-                <div>35</div>
-                <div>36</div>
-                <div>37</div>
-                <div>38</div>
-                <div>39</div>
-                <div>40</div>
-              </div> */}
-              Pas de d'activites
+              <table class="table-auto min-w-full bg-white border border-gray-300">
+                <thead>
+                  <tr>
+                    <th class="py-2">#</th>
+                    <th class="py-2">Classes</th>
+                    <th class="py-2">Mercredi 12</th>
+                    <th class="py-2">vendredi 14</th>
+                    <th class="py-2">mercredi 19</th>
+                    <th class="py-2">vendredi 21</th>
+                    <th class="py-2">Mercredi 26</th>
+                    <th class="py-2">vendredi 28</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="border px-4 py-2">1</td>
+                    <td class="border px-4 py-2">2nde</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">2</td>
+                    <td class="border px-4 py-2">5e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                    <td class="border px-4 py-2">Philo</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">3</td>
+                    <td class="border px-4 py-2">4e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                    <td class="border px-4 py-2">Philo</td>
+                  </tr>
+
+                  <tr>
+                    <td class="border px-4 py-2">4</td>
+                    <td class="border px-4 py-2">3e</td>
+                    <td class="border px-4 py-2">Anglais</td>
+                    <td class="border px-4 py-2">Français</td>
+                    <td class="border px-4 py-2">Hg</td>
+                    <td class="border px-4 py-2">Math</td>
+                    <td class="border px-4 py-2">Svt</td>
+                    <td class="border px-4 py-2">Philo</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </AccordionBody>
         </Accordion>
